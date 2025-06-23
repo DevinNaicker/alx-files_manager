fc448cf9c2414ed28f9333ea0953ee4252dd38b8
Devin Naicker <thirushandevinnaicker@gmail.com>
2025-06-23 15:28:06 +0200

    files manager 3

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,8 @@ import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
-// Status check routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-// New user registration route
 router.post('/users', UsersController.postNew);
 
 export default router;
