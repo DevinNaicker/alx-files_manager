826ac8cefde58b97e4b1b314950f258edcc7effa
Devin Naicker <thirushandevinnaicker@gmail.com>
2025-06-24 15:20:48 +0200

    files manager 0

--- utils/redis.js
+++ utils/redis.js
@@ -1,54 +1,35 @@
 import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = createClient();
     this.client.on('error', (err) => {
-      console.error('Redis Client Error:', err);
+      console.error(`Redis client error: ${err}`);
     });
 
-    this.client.connect()
-      .then(() => {
-        // connection successful
-      })
-      .catch((err) => {
-        console.error('Redis Connection Error:', err);
-      });
+    // Promisify Redis methods
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
-    return this.client.isOpen;
+    return this.client.connected;
   }
 
   async get(key) {
-    try {
-      const value = await this.client.get(key);
-      return value;
-    } catch (err) {
-      console.error(`Redis GET Error for key "${key}":`, err);
-      return null;
-    }
+    return this.getAsync(key);
   }
 
   async set(key, value, duration) {
-    try {
-      await this.client.set(key, value, {
-        EX: duration,
-      });
-    } catch (err) {
-      console.error(`Redis SET Error for key "${key}":`, err);
-    }
+    await this.setAsync(key, value, 'EX', duration);
   }
 
   async del(key) {
-    try {
-      await this.client.del(key);
-    } catch (err) {
-      console.error(`Redis DEL Error for key "${key}":`, err);
-    }
+    await this.delAsync(key);
   }
 }
 
 const redisClient = new RedisClient();
 export default redisClient;
-export { RedisClient };
